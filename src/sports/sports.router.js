9d23520bcf4339870c44aa7f33d2c14a4ef5db5d
Tyler Confalone <tconfalone@gmail.com>
2018-06-30 16:56:34 -0700

    2%

--- src/sports/sports.router.js
+++ src/sports/sports.router.js
@@ -2,18 +2,18 @@
 
 import express from 'express';
 const router = express.Router();
-import team from '../models/team/js';
-import auth from './middleware.js';
+import Team from '../models/teams.js';
+import auth from '../auth/middleware.js';
 
 
-router.get('/api/v1/team', auth, (req, res, next) => {
-  Teams.findOne({})
+router.get('/api/v1/teams', (req, res, next) => {
+  Team.find({})
     .then(data => sendJSON(res, data))
     .catch(next);
 });
 
-router.get('/api/v1/teams/:id', auth, (req, res, next) => {
-  Teams.findOne({_id: req.params.id})
+router.get('/api/v1/teams/:id', (req, res, next) => {
+  Team.findOne({_id: req.params.id})
     .then(data => sendJSON(res, data))
     .catch(next);
 });
@@ -26,7 +26,7 @@ router.put('/api/v1/:teams/:id', (req, res, next) => {
 
 });
 
-router.delete('/api/v1/:teams/:id', auth, (req, res, next) => {
+router.delete('/api/v1/:teams/:id', (req, res, next) => {
 
   req.model.findByIdAndDelete(req.params.id)
     .then(data => sendJSON(res, data))
